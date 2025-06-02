d38200d319e720301b1132c5908e9b8223c8a86b
denisYordanov-eng <denisivov437@gmail.com>
2025-06-02 18:07:42 +0300

    Made a task.

---
+++ 01-ExtractEmails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"(?<=\s)([a-z0-9])+([\.\-]\w*)*@([a-z])+([\.\-]\w*)*(\.[a-z]+)");
+
+            string inputText = Console.ReadLine();
+            var emailMatches = regex.Matches(inputText);
+
+            foreach (Match email in emailMatches)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+}
